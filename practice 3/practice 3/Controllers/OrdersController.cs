bbb380e79ce8daca7dbfeac07988e6b75f48ac32
gio choladze <gio.choladze2008@gmail.com>
2024-11-21 20:27:49 +0400

    Initial commit: branchebze ragaceebi ameria da controllerebi da modelebi dabrunda ukan

---
+++ practice 3/practice 3/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using practice_3.Models;
+using System.Diagnostics;
+
+namespace practice_3.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly string connectionstring = "Data Source=DESKTOP-BS2HUTQ\\SQLEXPRESS;Initial Catalog=orders;Integrated Security=True;Trust Server Certificate=TrueData Source=DESKTOP-BS2HUTQ\\SQLEXPRESS;Initial Catalog=orders;Integrated Security=True;Trust Server Certificate=True";
+       
+        private readonly ILogger<HomeController> _logger;
+
+        public OrdersController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
