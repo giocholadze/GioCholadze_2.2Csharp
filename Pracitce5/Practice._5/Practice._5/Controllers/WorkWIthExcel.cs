1ac01ede3535a65bb8879e111e707e7961aa9bda
gio choladze <gio.choladze2008@gmail.com>
2025-03-10 19:39:50 +0400

    Pracitice 5

---
+++ Pracitce5/Practice._5/Practice._5/Controllers/WorkWIthExcel.cs
@@ -0,0 +1,60 @@
+using ClosedXML.Excel;
+using System.ComponentModel;
+using System.Drawing;
+using System.Reflection;
+
+namespace ExcelImportExport.Controllers
+{
+    public class WorkWithExcel<T>
+    {
+        public IXLWorksheet AddHeader(XLWorkbook wb, List<T> objs)
+        {
+
+            T obj = objs.FirstOrDefault();
+            var ws = wb.Worksheets.Add(obj.GetType().Name).SetTabColor(XLColor.Green);
+            PropertyInfo[] properties = obj.GetType().GetProperties();
+
+            for (int i = 0; i < properties.Length; i++)
+            {
+                var displayNameAttribute = properties[i].GetCustomAttributes(typeof(DisplayNameAttribute), false);
+                string displayName = (displayNameAttribute.Count() != 0) ?
+                                     (displayNameAttribute[0] as DisplayNameAttribute).DisplayName : properties[0].Name;
+                ws.Cell(1, i + 1).Value = displayName;
+                ws.Cell(1, i + 1).Style.Font.Bold = true;
+            }
+
+            return ws;
+
+        }
+        public IXLWorksheet AddBody(IXLWorksheet ws, List<T> objs)
+        {
+            T obj = objs.FirstOrDefault();
+            int properties = obj.GetType().GetProperties().Length;
+            string previousValue = string.Empty;
+
+            for (int i = 0; i < objs.Count(); i++)
+            {
+                Type myType = objs[i].GetType();
+                PropertyInfo[] props = myType.GetProperties();
+
+                for (int j = 0; j < properties; j++)
+                {
+                    PropertyInfo prop = props[j];
+                    string propValue = prop.GetValue(objs[i], null).ToString();
+                    ws.Cell(2 + i, j + 1).Value = propValue;
+                }
+            }
+            return ws;
+        }
+        public XLWorkbook Generate(List<T> objs)
+        {
+            // Workbook creation.
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                var ws = AddHeader(wb, objs);
+                ws = AddBody(ws, objs);
+                return wb;
+            }
+        }
+    }
+}
---
+++ Pracitce5/Practice._5/Practice._5/Models/Employee.cs
@@ -0,0 +1,23 @@
+namespace Practice5.Models
+{
+    public class Employee
+    {
+        public string employeeID { get; set; }
+        public string gvari { get; set; }
+        public string saxeli { get; set; }
+        public string ganyofileba { get; set; }
+        public string qalaqi { get; set; }
+        public string regioni { get; set; }
+        public string raioni { get; set; }
+        public string xelfasi { get; set; }
+        public string asaki { get; set; }
+        public string staji { get; set; }
+        public string tarigi_dabadebis { get; set; }
+        public string sqesi { get; set; }
+        public string misamarti_saxlis { get; set; }
+        public string teleponi_saxlis { get; set; }
+        public string mobiluri { get; set; }
+        public string ierarqia { get; set; }
+
+    }
+}
